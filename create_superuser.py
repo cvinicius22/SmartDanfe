ef97dc69bfbc0ce7d374fc07dc259912a5bd4976
cvinicius22 <130810046+cvinicius22@users.noreply.github.com>
2026-03-30 01:19:09 -0300

    Update create_superuser.py

--- create_superuser.py
+++ create_superuser.py
@@ -7,9 +7,9 @@ django.setup()
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-username = os.environ.get('SUPERUSER_USERNAME', 'admin')
+username = os.environ.get('SUPERUSER_USERNAME', 'csilveira')
 email = os.environ.get('SUPERUSER_EMAIL', 'admin@example.com')
-password = os.environ.get('SUPERUSER_PASSWORD', 'admin123')
+password = os.environ.get('SUPERUSER_PASSWORD', 'Mariajulia')
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
