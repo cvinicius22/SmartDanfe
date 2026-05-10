eed230dd671691dfc85269125cd8d125f0fbd1ad
cvinicius22 <130810046+cvinicius22@users.noreply.github.com>
2026-05-10 00:35:16 -0300

    Add files via upload

--- meudanfe_project/settings.py
+++ meudanfe_project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
+from decouple import config  # se estiver usando python-decouple, ou use os.environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,12 +82,12 @@ WSGI_APPLICATION = 'meudanfe_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',   # fallback para desenvolvimento local
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
